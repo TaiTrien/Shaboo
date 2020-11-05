019a6728f42969e097fe5abab0b11a85556e7447
TaiTrien <v.v.t.trien@gmail.com>
2020-11-06 01:49:27 +0700

    fix: name of bloc folder

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,8 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shaboo/blocs/user/user_bloc.dart';
 import 'package:shaboo/routes.dart';
-
-import 'bloc/user/user_bloc.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
