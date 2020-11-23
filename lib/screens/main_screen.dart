a4ab0e1ce8ee2e585d1c722534abd56b3f192da1
TaiTrien <v.v.t.trien@gmail.com>
2020-11-23 22:25:58 +0700

    feat: change bottom navigation bar

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,8 +1,6 @@
-import 'package:fancy_bottom_navigation/fancy_bottom_navigation.dart';
 import 'package:flutter/material.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:shaboo/constants.dart';
-import 'package:shaboo/screens/auth/login/login_screen.dart';
 import 'package:shaboo/screens/auth/sign_up/sign_up_screen.dart';
 import 'package:shaboo/screens/home/home_screen.dart';
 import 'package:shaboo/screens/notification/notification_screen.dart';
@@ -14,7 +12,8 @@ class MainScreen extends StatefulWidget {
 
 class _MainScreenState extends State<MainScreen> {
   int _selectedIndex = 0;
-
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
   static List<Widget> _widgetOptions = <Widget>[
     HomeScreen(),
     SignupScreen(),
@@ -22,28 +21,88 @@ class _MainScreenState extends State<MainScreen> {
     HomeScreen(),
   ];
 
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: _widgetOptions.elementAt(_selectedIndex),
       ),
-      bottomNavigationBar: FancyBottomNavigation(
-        circleColor: kPrimaryColor,
-        inactiveIconColor: kSecondaryColor,
-        textColor: kPrimaryColor,
-        tabs: [
-          TabData(iconData: MdiIcons.home, title: "Home"),
-          TabData(iconData: MdiIcons.calendarMultipleCheck, title: "Events"),
-          TabData(iconData: MdiIcons.bell, title: "Notifications"),
-          TabData(iconData: Icons.account_circle, title: "Profile"),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: kPrimaryColor,
+        child: Icon(Icons.add),
+        onPressed: () {},
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(MdiIcons.calendarMultipleCheck),
+            label: 'Event',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(MdiIcons.bell),
+            label: 'Notifications',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
         ],
-        onTabChangedListener: (position) {
-          setState(() {
-            _selectedIndex = position;
-          });
-        },
+        currentIndex: _selectedIndex,
+        selectedItemColor: kPrimaryColor,
+        onTap: _onItemTapped,
       ),
     );
   }
 }
+// class MainScreen extends StatefulWidget {
+//   @override
+//   _MainScreenState createState() => _MainScreenState();
+// }
+
+// class _MainScreenState extends State<MainScreen> {
+//   int _selectedIndex = 0;
+
+//   static List<Widget> _widgetOptions = <Widget>[
+//     HomeScreen(),
+//     SignupScreen(),
+//     NotificationScreen(),
+//     HomeScreen(),
+//   ];
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Center(
+//         child: _widgetOptions.elementAt(_selectedIndex),
+//       ),
+//       bottomNavigationBar: FancyBottomNavigation(
+//         circleColor: kPrimaryColor,
+//         inactiveIconColor: kSecondaryColor,
+//         textColor: kPrimaryColor,
+//         tabs: [
+//           TabData(iconData: MdiIcons.home, title: "Home"),
+//           TabData(iconData: MdiIcons.calendarMultipleCheck, title: "Events"),
+//           TabData(iconData: MdiIcons.bell, title: "Notifications"),
+//           TabData(iconData: Icons.account_circle, title: "Profile"),
+//         ],
+//         onTabChangedListener: (position) {
+//           setState(() {
+//             _selectedIndex = position;
+//           });
+//         },
+//       ),
+//     );
+//   }
+// }
