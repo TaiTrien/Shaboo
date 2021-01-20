1cdcac822679772b7c7be12c834847badaf8aeb4
TaiTrien <v.v.t.trien@gmail.com>
2021-01-20 17:59:11 +0700

    fix: home screen

--- lib/screens/home/components/vertical_book_tile.dart
+++ lib/screens/home/components/vertical_book_tile.dart
@@ -14,21 +14,24 @@ class VerticalBookTile extends StatelessWidget {
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
     return Container(
-      margin: const EdgeInsets.only(right: 20.0),
       width: size.width / 3,
       child: ListTile(
         contentPadding: EdgeInsets.all(0),
         title: Container(
-          height: size.height / 5,
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(10.0),
-            child: Image.network(
-              imageLink,
-              fit: BoxFit.cover,
-              loadingBuilder: (BuildContext context, Widget child, ImageChunkEvent loadingProgress) {
-                if (loadingProgress == null) return child;
-                return LoadingWidget(isImage: true);
-              },
+          margin: EdgeInsets.symmetric(horizontal: 5),
+          child: Container(
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(5.0),
+              child: Image.network(
+                imageLink,
+                height: 150,
+                width: 100,
+                fit: BoxFit.cover,
+                loadingBuilder: (BuildContext context, Widget child, ImageChunkEvent loadingProgress) {
+                  if (loadingProgress == null) return child;
+                  return LoadingWidget(isImage: true);
+                },
+              ),
             ),
           ),
         ),
