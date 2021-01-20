863c020d57158c30d55f931a05a9bdfb8246e23e
TaiTrien <v.v.t.trien@gmail.com>
2021-01-20 21:56:20 +0700

    feat: add stream controller to post

--- lib/screens/post/post_controller.dart
+++ lib/screens/post/post_controller.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:shaboo/api/constants.dart';
@@ -7,8 +9,49 @@ import 'package:shaboo/screens/post/preview/preview_screen.dart';
 class PostController {
   BuildContext context;
   PageController pageController;
-  PostController({this.context}) {
+  Stream<List<dynamic>> postStream;
+  bool hasMore;
+  bool _isLoading;
+  int currentPage;
+  int bookId;
+  List<dynamic> _postList;
+  StreamController<List<dynamic>> _streamController;
+  ScrollController scrollController;
+
+  PostController({this.context, this.bookId}) {
     pageController = new PageController(initialPage: 0);
+    scrollController = ScrollController();
+    _postList = List<dynamic>();
+    _streamController = StreamController<List<dynamic>>.broadcast();
+    _isLoading = false;
+    postStream = _streamController.stream;
+    currentPage = 1;
+    hasMore = true;
+    refresh();
+  }
+  Future<void> refresh() => loadMore(clearCachedData: true);
+
+  Future<void> loadMore({bool clearCachedData = false, int currentPage}) async {
+    if (clearCachedData) {
+      _postList = List<dynamic>();
+      hasMore = true;
+      this.currentPage = 1;
+    }
+    if (_isLoading || !hasMore) return Future.value();
+
+    _isLoading = true;
+    this.currentPage++;
+
+    try {
+      return await ListPost.getPosts(eOrder: EOrder.ASC, page: currentPage, take: 10, bookId: bookId).then((post) => {
+            _isLoading = false,
+            _postList.addAll(post.listPost),
+            hasMore = (_postList.length < post.itemCount),
+            _streamController.sink.add(_postList),
+          });
+    } catch (e) {
+      print(e);
+    }
   }
 
   dispose() {
@@ -22,7 +65,7 @@ class PostController {
   toPreviousPage() => pageController.previousPage(duration: Duration(milliseconds: 500), curve: Curves.ease);
   toExit() => Navigator.pop(context);
 
-  getPost(int bookId) async => await ListPost.getPosts(eOrder: EOrder.ASC, page: 1, take: 10, bookId: bookId);
+  getPost() async => await ListPost.getPosts(eOrder: EOrder.ASC, page: 1, take: 10, bookId: bookId);
 
   get defaultImage => "https://bitly.com.vn/pdmjnn";
 }
